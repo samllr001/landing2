84daf6b754eae5fe01c4a1882e19ab4f56c67735
samllr001 <saminmirakubov@gmail.com>
2024-04-02 15:15:48 +0500

    update site

--- js/script.js
+++ js/script.js
@@ -30,3 +30,19 @@ function twoDigitsNum(a) {
         return a
     }
 }
+
+
+//PopUp Start
+
+
+let whiteBtn = document.getElementsByClassName("white")[0]
+let popUpWrapper = document.querySelector(".popUpForm")
+let closeBtn = document.querySelector(".close")
+whiteBtn.addEventListener("click", popUpForm);
+closeBtn.addEventListener("click", closePopUp)
+function popUpForm() {
+    popUpWrapper.style.display="flex"
+}
+function closePopUp() {
+    popUpWrapper.style.display="none";
+}
